f907e777493f8cd393a11986b1bb6274537803ae
Lucas pinheiro <lucas.pinheiro@autoglass.com.br>
2024-05-10 15:29:28 -0300

    update

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -6,7 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listar-pensamento.component.css'],
 })
 export class ListarPensamentoComponent implements OnInit {
-  listaPensamentos = [];
+  listaPensamentos = [
+    {
+      conteudo: 'Comunicação entre componentes',
+      autoria: 'Angular',
+      modelo: 'modelo2',
+    },
+    {
+      conteudo: 'Comunicação entre componentes',
+      autoria: 'Angular',
+      modelo: 'modelo1',
+    },
+    {
+      conteudo:
+        'A transformação digital reconfigurou a paisagem econômica impulsionando a automação e a inovação em todos os setores A inteligência artificial a análise de dados e a Internet das Coisas são os pilares dessa mudança promovendo eficiência personalização e novas oportunidades de negócios Essa revolução tecnológica está moldando o futuro trazendo desafios e possibilidades sem precedentes,',
+      autoria: 'Angular',
+      modelo: 'modelo3',
+    },
+  ];
   constructor() {}
 
   ngOnInit(): void {}
